b95854375daca433667be017dfab4e2e149b1d30
Fadigafr <96269999+Fadigafr@users.noreply.github.com>
2026-06-13 21:41:13 +0400

    Refactor markdown display for match analysis
    Signed-off-by: Fadigafr <96269999+Fadigafr@users.noreply.github.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,19 @@ import numpy as np
 
 st.set_page_config(page_title="BET AI PREMIUM", layout="wide")
 
-st.markdown(f"""
-<div class="card">
-    <b>{team1} vs {team2}</b><br><br>
+for team1, team2 in matches:
 
-    <b>Probabilité :</b> {prob1}% | {probX}% | {prob2}%<br>
-    <b>Cote :</b> {odd}<br>
-    <b>Tip :</b> <span style="color:green; font-weight:bold">{tip}</span>
-</div>
+    prob1, probX, prob2, odd, tip = analyse(team1, team2)
+
+    st.markdown(f"""
+    <div class="card">
+        <b>{team1} vs {team2}</b><br><br>
+
+        <b>Probabilité :</b> {prob1}% | {probX}% | {prob2}%<br>
+        <b>Cote :</b> {odd}<br>
+        <b>Tip :</b> <span style="color:green; font-weight:bold">{tip}</span>
+    </div>
+    """, unsafe_allow_html=True)
 """, unsafe_allow_html=True)
 st.markdown("""
 <style>
